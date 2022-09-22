e59453dc4bdca1dc7c20598edbbe0cd10a0718f3
Leandro <leo250703@gmail.com>
2022-09-22 09:16:31 -0400

    Commit 22/09/2022 09:15 (Leandro Morales) -> agregar un handler para la barra de progreso

--- app/src/main/java/com/example/ejemplohandler/MainActivity.kt
+++ app/src/main/java/com/example/ejemplohandler/MainActivity.kt
@@ -3,16 +3,25 @@ package com.example.ejemplohandler
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.ejemplohandler.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var myHandler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnStart.setOnClickListener { passScreen() }
+
+        //Inicializar handler
+
+        myHandler = Handler(mainLooper)
+        binding.btnStart.setOnClickListener {
+            loadScreen()
+            //passScreen()
+        }
     }
 
     private fun passScreen() {
@@ -33,7 +42,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun loadScreen() {
         Thread{
-
+            try {
+                for (i in 0 .. 100){
+                    Thread.sleep(500)
+                    myHandler.post{
+                        binding.apply {
+                            tvPercentage.text = "$i%"
+                            pbProgress.progress = i
+                        }
+                    }
+                }
+            } catch (e: InterruptedException){
+                e.printStackTrace()
+            }
         }.start()
     }
 }
